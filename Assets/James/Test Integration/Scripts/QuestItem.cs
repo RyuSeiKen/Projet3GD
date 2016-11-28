a7db6253337321356d913ffd9e109b79c5084b1f
James Petit <jp.james.petit@gmail.com>
2016-11-28 22:15:19 +0100

    Objets + Gestion de Quete

---
+++ Assets/James/Test Integration/Scripts/QuestItem.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuestItem : MonoBehaviour 
+{
+	bool takeAble = false;
+
+	void Update () 
+	{
+		transform.Rotate(Vector3.up * Time.deltaTime * 100);
+	}
+
+	void OnTriggerEnter () 
+	{
+		if(takeAble)
+		{
+			gameObject.SetActive(false);
+		}
+	}
+}
---
+++ Assets/James/Test Integration/Scripts/QuestManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuestManager : MonoBehaviour 
+{	
+	PlayerMembership player;
+
+	void Start() 
+	{
+		player = FindObjectOfType<PlayerMembership>();
+	}
+
+	public void GreenQuest()
+	{
+		if(player.currentQuest == "Green")
+		{
+			Debug.Log("You've already accepted the green quest.");
+			return;
+		}
+		if(player.currentQuest != "None" && player.currentQuest != "Green")
+		{
+			Debug.Log(player.currentQuest + " quest abandoned.");
+		}
+		Debug.Log("Find the item we ask for.");
+		player.currentQuest = "Green";
+	}
+
+	public void BlueQuest()
+	{
+		if(player.currentQuest == "Blue")
+		{
+			Debug.Log("You've already accepted the blue quest.");
+			return;
+		}
+		if(player.currentQuest != "None" && player.currentQuest != "Blue")
+		{
+			Debug.Log(player.currentQuest + " quest abandoned.");
+		}
+		Debug.Log("Answer the question correctly.");
+		player.currentQuest = "Blue";
+	}
+
+	public void RedQuest()
+	{
+		if(player.currentQuest == "Red")
+		{
+			Debug.Log("You've already accepted the red quest.");
+			return;
+		}
+		if(player.currentQuest != "None" && player.currentQuest != "Red")
+		{
+			Debug.Log(player.currentQuest + " quest abandoned.");
+		}
+		Debug.Log("Attack another faction.");
+		player.currentQuest = "Red";
+	}
+}
